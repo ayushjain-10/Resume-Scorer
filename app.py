8692a5e64dc302d817256c87bed1301ee4433e88
Ayush Jain <ayushshreeshreemal@gmail.com>
2024-12-01 11:31:04 -0500

    updates

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://resume-scorer-three.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
