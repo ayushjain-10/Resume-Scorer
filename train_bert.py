6ff9bfb6abff0f9b6b60322e2086f569e55e46b8
Ayush Jain <ayushshreeshreemal@gmail.com>
2024-12-01 10:06:03 -0500

    updates

--- train_bert.py
+++ train_bert.py
@@ -214,6 +214,4 @@ print(f"Predicted Class: {predicted_class.item()}")
 predicted_category = label_encoder.inverse_transform([predicted_class.item()])
 print(f"Predicted Category: {predicted_category[0]}")
 
-#######Example:
-Predicted Class: 0
-Predicted Category: Accountant
+#######Example
